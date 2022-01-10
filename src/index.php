5287d432670d070ea32a1a92cd9cf2cd6fcade2d
Mexknak <tony.paques@orange.fr>
2022-01-10 16:51:35 +0100

    2

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-echo "OOUOOOOHHHH";
+echo "OOUOOOOHHHH2";
 echo '<p>update : 10 janvier 2022 </p>';
 // phpinfo();
 ?>
